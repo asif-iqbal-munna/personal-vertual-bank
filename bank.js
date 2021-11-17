916cc1b2c9f83e4d39ffa703f4c18bf38a64a872
asif-iqbal-munna <asifiqbalmunna1318@gmail.com>
2021-11-18 02:05:48 +0600

    validated and responsive

--- bank.js
+++ bank.js
@@ -1,32 +1,47 @@
-function bankTransection(transectionButtonId, userTransectionFieldId, transectionStatusHoldingId, isAdd){
-    document.getElementById(transectionButtonId).addEventListener('click', function(){
-        // debugger;
-        // getting new value from the input fields
-        const userTransectionAmount = parseFloat(document.getElementById(userTransectionFieldId).value);
-        
-        // getting value of current deposit and withdrawal balance
-        const totalTransectionBalance = document.getElementById(transectionStatusHoldingId);
-        const transectionStatus = parseFloat(totalTransectionBalance.innerText);
-
-        // adding input and current values and show it to current deposit or withdrawal balance
-        const totalTransectionAmount = userTransectionAmount + transectionStatus;
-        totalTransectionBalance.innerText = totalTransectionAmount;
-        // making the input field empty
-        document.getElementById(userTransectionFieldId).value = "";
-
-        // getting the current balance adding new deposit amount and subtracting withdrawal amount
-        const getCurrentBalance = document.getElementById('current-balance');
-        if(isAdd == true){ // adding new deposit amount if true
-            const currentBalance = parseFloat(getCurrentBalance.innerText);
-            const totalCurrentBalance = currentBalance + userTransectionAmount;
-            getCurrentBalance.innerText = totalCurrentBalance;
-        } else { // subtracting withdrawal amount if false
-            const currentBalance = parseFloat(getCurrentBalance.innerText);
-            const totalCurrentBalance = currentBalance - userTransectionAmount;
-            getCurrentBalance.innerText = totalCurrentBalance;
-        }
-    })
+function getInputValue(inputFieldId) {
+  // debugger;
+  const inputField = document.getElementById(inputFieldId);
+  const inputAmount = parseInt(inputField.value);
+  inputField.value = "";
+  return inputAmount;
 }
 
-bankTransection('deposit-btn', 'user-deposited-amount', 'deposit-balance', true)
-bankTransection('withdrawal-btn', 'user-withdrawal-amount', 'withdrawal-balance', false)
+function updateTotalBalance(currentTotalId, inputAmount) {
+  const catchTotal = document.getElementById(currentTotalId);
+  const total = parseInt(catchTotal.innerText);
+  catchTotal.innerText = total + inputAmount;
+}
+
+function getCurrentBalance() {
+  const catchBalance = document.getElementById("current-balance");
+  const previousBalanceAmount = parseInt(catchBalance.innerText);
+  return previousBalanceAmount;
+}
+
+function updateMainBalance(inputAmount) {
+  const catchBalance = document.getElementById("current-balance");
+  const previousBalanceAmount = getCurrentBalance();
+  catchBalance.innerText = previousBalanceAmount + inputAmount;
+}
+
+document.getElementById("deposit-btn").addEventListener("click", function () {
+  const inputAmount = getInputValue("user-deposited-amount");
+  if (inputAmount > 0) {
+    updateTotalBalance("deposit-balance", inputAmount);
+    updateMainBalance(inputAmount);
+  } else {
+    alert("Please Enter Valid Amount");
+  }
+});
+document
+  .getElementById("withdrawal-btn")
+  .addEventListener("click", function () {
+    const inputAmount = getInputValue("user-withdrawal-amount");
+    const currentBalance = getCurrentBalance();
+    if (inputAmount > 0 && inputAmount < currentBalance) {
+      updateTotalBalance("withdrawal-balance", inputAmount);
+      updateMainBalance(-inputAmount);
+    } else {
+      alert("You Don't Have That Much Balance For Withdrawal");
+    }
+  });
